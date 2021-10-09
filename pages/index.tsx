e0a940121159be59aa90f50a91d56cfd50df0598
Christian Kozalla <christian.kozalla@gmail.com>
2021-10-09 09:07:15 +0200

    Spins up "/" route and redirects "/home" route to it - prevents duplicate pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import type { GetStaticProps, NextPage } from "next";
-import { API_BASE_URL } from "../lib/api";
+import { getContent, Page, Params } from "../lib/api";
 
 export const getStaticProps: GetStaticProps = async () => {
+  const [pageContent] = await getContent("pages?slug=home");
+
   return {
-    props: {},
+    props: {
+      page: pageContent,
+    },
   };
 };
 
-const Home: NextPage = () => {
-  return <div>Hello from Home</div>;
+const Home: NextPage<{ page: Page }> = ({ page }) => {
+  return (
+    <div>
+      <h1>Hello from Page {page.slug}</h1>
+      <h2 dangerouslySetInnerHTML={{ __html: page.title }} />
+      <div dangerouslySetInnerHTML={{ __html: page.exerpt }} />
+      <article dangerouslySetInnerHTML={{ __html: page.content }}></article>
+    </div>
+  );
 };
 
 export default Home;
