7e02725140d6a6912e7d0ec64ced6b0de9104897
Christian Kozalla <christian.kozalla@gmail.com>
2021-10-20 19:48:51 +0200

    Initial Jumbotron component with decorator

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -5,9 +5,40 @@ const Jumbotron: FunctionComponent<{
   decorator?: { url: string };
 }> = ({ image, decorator }) => {
   return (
-    <div>
-      <img src={image.url} alt={image.alt} />
-      {decorator && <img src={decorator.url} alt="Image Decorator Logo" />}
+    <div className="jumbotron__wrapper">
+      <img className="jumbotron" src={image.url} alt={image.alt} />
+      {decorator && (
+        <img
+          className="decorator"
+          src={decorator.url}
+          alt="Image Decorator Logo"
+        />
+      )}
+      <style jsx>{`
+        @import "../styles/styles";
+
+        .jumbotron__wrapper {
+          position: relative;
+        }
+
+        .jumbotron {
+          display: block;
+          width: 100%;
+        }
+
+        .decorator {
+          position: absolute;
+          right: 5%;
+          top: 25%;
+          height: 50%;
+        }
+
+        @media only screen and (max-width: 769px) {
+          .decorator {
+            display: none;
+          }
+        }
+      `}</style>
     </div>
   );
 };
